e1e4c54a9f4e06d8c48ada2be62cf3d49db69891
afmelo92 <andre.fabian.melo@gmail.com>
2020-01-20 21:50:51 -0400

    Add session

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,12 +2,19 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    const { name, email, password_hash } = await User.create(req.body);
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
+    const { id, name, email, provider } = await User.create(req.body);
 
     return res.json({
+      id,
       name,
       email,
-      password_hash,
+      provider,
     });
   }
 }
